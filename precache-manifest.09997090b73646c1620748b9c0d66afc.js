7bb6020f5e06eb07c397d86a54ff813ea2532b0d
soomroshoaib <soomroshoaib178@gmail.com>
2021-03-03 05:12:08 -0800

    Updates

---
+++ precache-manifest.09997090b73646c1620748b9c0d66afc.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a1c694f2a032f9d47c04591f814d3a85",
+    "url": "/Ecommerce-react-redux/index.html"
+  },
+  {
+    "revision": "336d41258e293cb52fe3",
+    "url": "/Ecommerce-react-redux/static/css/2.5c2d44b2.chunk.css"
+  },
+  {
+    "revision": "b75f110481d109c26c82",
+    "url": "/Ecommerce-react-redux/static/css/main.3a11e748.chunk.css"
+  },
+  {
+    "revision": "336d41258e293cb52fe3",
+    "url": "/Ecommerce-react-redux/static/js/2.8636a8c7.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/Ecommerce-react-redux/static/js/2.8636a8c7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b75f110481d109c26c82",
+    "url": "/Ecommerce-react-redux/static/js/main.922bb0d6.chunk.js"
+  },
+  {
+    "revision": "5efb8dba30d0172bbd78",
+    "url": "/Ecommerce-react-redux/static/js/runtime-main.a24cec0e.js"
+  },
+  {
+    "revision": "0210e597f574b4bd3f751ca6b3629f1c",
+    "url": "/Ecommerce-react-redux/static/media/bbb.0210e597.jpg"
+  },
+  {
+    "revision": "59731bae1af505b797e0bab856268623",
+    "url": "/Ecommerce-react-redux/static/media/dslr.59731bae.jpg"
+  },
+  {
+    "revision": "8d318e4a591da8b33dddbe1467a809c9",
+    "url": "/Ecommerce-react-redux/static/media/iphone.8d318e4a.jpg"
+  },
+  {
+    "revision": "e7bdef88d0ff67525ebeb96877270e62",
+    "url": "/Ecommerce-react-redux/static/media/microphone.e7bdef88.jpg"
+  },
+  {
+    "revision": "92ef6ab7b31320d776128b8bf7944473",
+    "url": "/Ecommerce-react-redux/static/media/perfume.92ef6ab7.jpg"
+  },
+  {
+    "revision": "986af3717a208f0b993fffa0d353abe6",
+    "url": "/Ecommerce-react-redux/static/media/rings.986af371.jpg"
+  },
+  {
+    "revision": "f30fe0b2e59c7281ecf5bd07d202a595",
+    "url": "/Ecommerce-react-redux/static/media/shoes.f30fe0b2.jpg"
+  },
+  {
+    "revision": "b4e58e91e391491d2836ce808e677037",
+    "url": "/Ecommerce-react-redux/static/media/watch.b4e58e91.jpg"
+  }
+]);
